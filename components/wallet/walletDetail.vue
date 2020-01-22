8d20ec4fb9e16ad37bf1eb1cc09312619bd04fe7
krispea3 <christian.bracher@gmail.com>
2020-01-22 13:38:36 +0800

    Added WalletDetail component and WalletList

--- components/wallet/walletDetail.vue
+++ components/wallet/walletDetail.vue
@@ -1,144 +1,51 @@
 <template>
-  <v-card class="mt-2">
-    <!-- <v-img
-      :src="require('../../assets/images/cryptowallet.jpg')" aspect-ratio="4"
-    ></v-img> -->
-    <v-layout row justify-center>
-      <v-card-title>
-        <h3>Active Wallet</h3>
-      </v-card-title>
-    </v-layout>
-    <v-card-text>
-        <v-layout row wrap>
-          <v-flex xs1>
-            <!-- <v-layout> -->
-              <v-checkbox
-                multiple
-                v-model="selected"
-                :value="activePrivateKey"
-                color="red"
-                hide-details
-                class="shrink mr-2"
-              >
-              </v-checkbox>
-          </v-flex>
-          <v-flex xs10 pr-4>
-              <v-textarea
-                label="Private Key"
-                readonly
-                rows=1
-                auto-grow
-                outline
-                background-color="green"
-                :value="activePrivateKey"
-              ></v-textarea>
-            <!-- </v-layout> -->
-          </v-flex>
-          <v-flex xs1>
-            <v-text-field
-              label="Balance"
-              readonly
-              :value="activeWalletBalance"
-            >
-            </v-text-field>
-          </v-flex>
-        </v-layout>
-        
-        <v-layout row wrap justify-center>
-          <v-flex xs10 pr-4>
-            <v-textarea
-              label="Public Key"
-              readonly
-              rows=1
-              auto-grow
-              outline
-              background-color="green"
-              :value="activeWalletAddress"
-            ></v-textarea>
-            <hr class="mt-3">
-          </v-flex>
-        </v-layout>
-    </v-card-text>
-
-    <h3 class="text-xs-center" v-if="walletAddresses.length > 1">Additional wallets</h3>
-    <v-card-text
-      class="text-xs-center"
-      v-for="(privateKey, index) in walletAddressesWithoutActiveAddress"
-      :key="index"
-    >
-      <v-layout row wrap>
-        <v-flex xs1>
-          <!-- <v-layout> -->
-            <v-checkbox
-              multiple
-              v-model="selected"
-              :value="privateKey"
-              color="red"
-              hide-details
-              class="shrink mr-2"
-            >
-            </v-checkbox>
-        </v-flex>
-        <v-flex xs10 pr-4>
-            <v-textarea
-              label="Private Key"
-              readonly
-              rows=1
-              auto-grow
-              outline
-              :value="privateKey"
-            ></v-textarea>
-        
-          <!-- </v-layout> -->
-        </v-flex>
-        <v-flex xs1>
-          <v-text-field
-            label="Balance"
-            readonly
-            :value="balanceOfAddresses[index]"
-          >
-          </v-text-field>
-        </v-flex>
-      </v-layout>
-
-      <v-layout row wrap justify-center>
-        <v-flex xs10 pr-4>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs1>
+        <v-checkbox
+          v-model="checkbox"
+          color="red"
+          class="shrink mr-2"
+        >
+        </v-checkbox>
+      </v-flex>
+      <v-flex xs10 pr-4>
           <v-textarea
-            label="Public Key"
+            label="Private Key"
             readonly
             rows=1
             auto-grow
             outline
-            :value="getPublicKey(privateKey)"
+            :background-color="privateKeyObj.privateKey === activePrivateKey ? 'green' : ''"
+            :value="privateKeyObj.privateKey"
           ></v-textarea>
-          <hr class="mt-3">
-        </v-flex>
-      </v-layout>
-    </v-card-text>
-
-    <v-card-actions class="justify-space-around">
-      <v-btn
-        @click="createWallet"
-        color="primary"
-      >
-        Generate Wallet
-      </v-btn>
-      <v-btn
-        @click="deleteWallet"
-        color="error"
-        :disabled="selected.length === 0"
-      >
-        Delete Wallet
-      </v-btn>
-      <v-btn
-        @click="switchActiveWallet"
-        color="green"
-        :disabled="selected.length !== 1"
-      >
-        Switch Active Wallet
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+        <!-- </v-layout> -->
+      </v-flex>
+      <v-flex xs1>
+        <v-text-field
+          label="Balance"
+          readonly
+          :value="privateKeyObj.balance"
+        >
+        </v-text-field>
+      </v-flex>
+    </v-layout>
+    
+    <v-layout row wrap justify-center>
+      <v-flex xs10 pr-4>
+        <v-textarea
+          label="Public Key"
+          readonly
+          rows=1
+          auto-grow
+          outline
+          :background-color="privateKeyObj.privateKey === activePrivateKey ? 'green' : ''"
+          :value="getPublicKey(privateKeyObj.privateKey)"
+        ></v-textarea>
+        <!-- <hr class="mt-3"> -->
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -146,75 +53,39 @@ const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 
 export default {
-
-  created () {
-    this.walletAddresses.forEach(privateKey => {
-      const publicKey = this.getPublicKey(privateKey);
-      this.$store.dispatch('getBalancebyAddress', publicKey)
-    })    
+    
+  props: {
+    privateKeyObj: { // privateKey, balance
+      type: Object,
+      required: true
+    }
   },
 
   data () {
     return {
-      selected: [],
+      checkbox: false,
+    }
+  },
+
+  watch: {
+    checkbox: function(val) {      
+      const type = val === true ?'add' :'remove';
+      const data = {
+        privateKey: this.privateKeyObj.privateKey,
+        type: type
+      };
+
+      this.$emit('changeSelection', data)
     }
   },
 
   computed: {
-    walletAddresses () {
-      return this.$store.getters.walletAddresses;
-    },
-
-    walletAddressesWithoutActiveAddress () {
-      return this.$store.getters.walletAddresses.filter(privateKey => privateKey !== this.activePrivateKey);
-    },
-
-    activeWalletAddress () {
-      return this.$store.getters.walletAddress;
-    },
-
     activePrivateKey () {      
       return this.$store.getters.privateKey
     },
-
-    activeWalletBalance () {
-      return this.$store.getters.balance;
-    },
-
-    balanceOfAddresses () {
-      return this.$store.getters.balanceOfAddresses;
-    }
   },
 
   methods: {
-    createWallet () {
-      console.log("generate wallet clicked");  
-      this.$emit('createWallet');
-    },
-
-    deleteWallet () {
-      this.selected.forEach(privateKey => {
-        const index = this.walletAddresses.indexOf(privateKey);
-        const balance = this.balanceOfAddresses[index];
-        if (balance > 0) {
-          console.log("Wallet has balance! Show dialog");
-        }else {
-          this.$emit("deleteWallet", {privateKey: privateKey});
-        }
-      })
-      this.selected = [];
-    },
-
-    switchActiveWallet () {
-      const index = this.selected.indexOf(this.activePrivateKey);
-      if (index < 0) {
-        this.$emit("switchActiveWallet", {privateKey: this.selected[0]});
-        this.selected = [];
-      }else {
-        console.log("Selected wallet is already active");
-      }
-    },
-
     getPublicKey (privateKey) {
       const keys = ec.keyFromPrivate(privateKey);
       return keys.getPublic('hex');
---
+++ components/wallet/walletList.vue
@@ -0,0 +1,165 @@
+<template>
+  <v-card class="mt-2">
+    <!-- Active wallet -->
+    <v-layout row justify-center>
+      <p>Selected: {{ selected }}</p>
+      <v-card-title>
+        <h3>Active Wallet</h3>
+      </v-card-title>
+    </v-layout>
+    <v-card-text>
+      <v-layout row wrap>
+        <v-flex xs12 pr-4>
+          <WalletDetail
+            :privateKeyObj="{privateKey: activePrivateKey, balance: activeWalletBalance}"
+            @changeSelection="changeSelection"  
+          />
+        </v-flex>
+      </v-layout>    
+    </v-card-text>
+    <!-- Additional Wallets -->
+    <div v-if="walletAddresses.length > 1">
+      <h3 class="text-xs-center">Additional wallets</h3>
+      <v-card-text
+        class="text-xs-center"
+        v-for="(privateKey, index) in walletAddressesWithoutActiveAddress"
+        :key="index"
+      >
+        <v-layout row wrap>
+          <v-flex xs12 pr-4>
+            <WalletDetail
+              :privateKeyObj="{privateKey: privateKey, balance: balanceOfAddresses[index]}"
+              @changeSelection="changeSelection"  
+            />
+          </v-flex>
+        </v-layout>
+      </v-card-text>
+    </div>
+    <!-- Actions -->
+    <v-layout row wrap justify-center>
+      <v-flex xs12>  
+        <div class="justify-space-around">
+          <v-btn
+            @click="createWallet"
+            color="primary"
+          >
+            Generate Wallet
+          </v-btn>
+          <v-btn
+            @click="deleteWallet"
+            color="error"
+            :disabled="selected.length === 0"
+          >
+            Delete Wallet
+          </v-btn>
+          <v-btn
+            @click="switchActiveWallet"
+            color="green"
+            :disabled="selected.length !== 1"
+          >
+            Switch Active Wallet
+          </v-btn>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-card>
+</template>
+
+<script>
+import WalletDetail from '../../components/wallet/walletDetail'
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+export default {
+  components: {
+    WalletDetail,
+  },
+
+  created () {
+    this.walletAddresses.forEach(privateKey => {
+      const publicKey = this.getPublicKey(privateKey);
+      this.$store.dispatch('getBalancebyAddress', publicKey)
+    })    
+  },
+
+  data () {
+    return {
+      selected: [],
+    }
+  },
+
+  computed: {
+    walletAddresses () {
+      return this.$store.getters.walletAddresses;
+    },
+
+    walletAddressesWithoutActiveAddress () {
+      return this.$store.getters.walletAddresses.filter(privateKey => privateKey !== this.activePrivateKey);
+    },
+
+    activeWalletAddress () {
+      return this.$store.getters.walletAddress;
+    },
+
+    activePrivateKey () {
+      console.log("activePrivateKey: ", this.$store.getters.privateKey);
+           
+      return this.$store.getters.privateKey
+    },
+
+    activeWalletBalance () {
+      return this.$store.getters.balance;
+    },
+
+    balanceOfAddresses () {
+      return this.$store.getters.balanceOfAddresses;
+    }
+  },
+
+  methods: {
+    createWallet () {
+      console.log("generate wallet clicked");  
+      this.$emit('createWallet');
+    },
+
+    deleteWallet () {
+      this.selected.forEach(privateKey => {
+        const index = this.walletAddresses.indexOf(privateKey);
+        const balance = this.balanceOfAddresses[index];
+        if (balance > 0) {
+          console.log("Wallet has balance! Show dialog");
+        }else {
+          this.$emit("deleteWallet", {privateKey: privateKey});
+        }
+      })
+      this.selected = [];
+    },
+
+    switchActiveWallet () {
+      const index = this.selected.indexOf(this.activePrivateKey);
+      if (index < 0) {
+        this.$emit("switchActiveWallet", {privateKey: this.selected[0]});
+        this.selected = [];
+      }else {
+        console.log("Selected wallet is already active");
+      }
+    },
+
+    getPublicKey (privateKey) {
+      const keys = ec.keyFromPrivate(privateKey);
+      return keys.getPublic('hex');
+    },
+
+    changeSelection (data) {      
+      if (data.type === 'add') {
+        this.selected.push(data.privateKey);
+      }else {
+        const index = this.selected.indexOf(data.privateKey);
+        if (index >= 0) {
+          this.selected.splice(index, 1);
+        }
+      }
+    }
+  }
+}
+</script>
