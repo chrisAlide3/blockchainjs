7c59e333afd256466bc2b10f0c04efc8f5c4e380
krispea3 <christian.bracher@gmail.com>
2020-01-28 10:31:32 +0800

    Added basic Mine route

---
+++ pages/mine/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <h1>Mine Route</h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      mined: false,
+    }
+  },
+  
+}
+</script>
