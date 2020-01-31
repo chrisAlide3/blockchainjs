610ae65ceb8ff02d99670bc7ffe15ef6e513a1f9
krispea3 <christian.bracher@gmail.com>
2020-01-31 11:50:25 +0800

    Outsources Private-/Public key handling to Component

---
+++ components/wallet/PrivateKey.vue
@@ -0,0 +1,119 @@
+<template>
+  <div>
+    <v-layout row>
+      <v-flex xs11 pr-2>
+        <v-text-field
+          v-model="password"
+          :append-icon="show1 ? 'visibility' : 'visibility_off'"
+          :type="show1 ? 'text' : 'password'"
+          label="Private Key"
+          readonly
+          @click:append="show1 = !show1"
+        >
+        </v-text-field>
+        <!-- Tooltip for Copy message -->
+        <v-layout justify-end>
+          <v-flex xs1>
+            <v-tooltip v-model="showCopyMessage" bottom>
+              <template v-slot:activator="{ on }">
+                <span v-on="on"></span>
+              </template>
+              <span>Private Key copied to clipboard</span>
+              <v-icon small right @click="showCopyMessage=false">close</v-icon>
+            </v-tooltip>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+
+      <v-flex xs1 pt-4>
+        <v-icon small @click="copyPrivateKey">mdi-content-copy</v-icon>
+        <v-icon small @click="qrcodeDialog = true">mdi-barcode</v-icon>
+      </v-flex>
+    </v-layout>
+
+    <!-- QR-Code dialog -->
+    <div class="text-xs-center">
+      <v-dialog
+        v-model="qrcodeDialog"
+        width="500"
+      >
+        <v-card>
+          <v-card-title
+            class="headline orange justify-center"
+            primary-title
+          >
+            QR-Code for Private-Key
+          </v-card-title>
+
+          <v-card-text class="text-xs-center" >
+            <qrcode-vue :value="privateKey" :size="300" level="H"></qrcode-vue>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions class="hidden-print-only">
+            <v-spacer></v-spacer>
+            <v-btn
+              color="orange"
+              flat
+              @click="printQrcode"
+            >
+              Print
+            </v-btn>
+            <v-btn
+              color="primary"
+              flat
+              @click="qrcodeDialog = false"
+            >
+              Close
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
+  </div>  
+</template>
+
+<script>
+import QrcodeVue from 'qrcode.vue'
+
+export default {
+  components: {
+    QrcodeVue,
+  },
+
+  props: {
+    privateKey: {
+      type: String,
+      required: true
+    },
+  },
+
+  data () {
+    return {
+      show1: false,
+      password: this.privateKey,
+      showCopyMessage: false,
+      qrcodeDialog: false,
+    }
+  },
+
+  methods: {
+    copyPrivateKey () {
+      try {
+        this.$copyText(this.privateKey);
+        this.showCopyMessage = true;
+        setTimeout(() => {
+          this.showCopyMessage = false
+        }, 3000);
+      } catch (error) {
+        alert('Could not copy key to clipboard');
+      }
+    },
+
+    printQrcode () {
+      window.print();
+    }
+  }
+}
+</script>
--- components/wallet/walletDetail.vue
+++ components/wallet/walletDetail.vue
@@ -9,36 +9,11 @@
         >
         </v-checkbox>
       </v-flex>
-      <v-flex xs9 pr-2>
-        <v-text-field
-          v-model="password"
-          :append-icon="show1 ? 'visibility' : 'visibility_off'"
-          :type="show1 ? 'text' : 'password'"
-          label="Private Key"
-          readonly
-          @click:append="show1 = !show1"
-        >
-        </v-text-field>
-        <!-- Tooltip for Copy message -->
-        <v-layout justify-end>
-          <v-flex xs1>
-            <v-tooltip v-model="showPrivateKeyCopyMessage" bottom>
-              <template v-slot:activator="{ on }">
-                <span v-on="on"></span>
-              </template>
-              <span>Private Key copied to clipboard</span>
-              <v-icon small right @click="showPrivateKeyCopyMessage=false">close</v-icon>
-            </v-tooltip>
-          </v-flex>
-        </v-layout>
-        
-      </v-flex>
 
-      <v-flex xs1 pt-4 grow>
-        <v-icon small @click="copyPrivateKey">mdi-content-copy</v-icon>
-        <v-icon small @click="showQrcode('privateKey')">mdi-barcode</v-icon>
+      <v-flex xs10>
+        <PrivateKey :privateKey="privateKeyObj.privateKey"/>
       </v-flex>
-
+      
       <v-flex xs1>
         <v-text-field
           label="Balance"
@@ -51,90 +26,23 @@
     </v-layout>
     
     <v-layout row wrap justify-center>
-      <v-flex xs9 pr-4>
-        <v-textarea
-          label="Public Key"
-          readonly
-          rows=1
-          auto-grow
-          outline
-          :background-color="privateKeyObj.privateKey === activePrivateKey ? 'green' : ''"
-          :value="getPublicKey(privateKeyObj.privateKey)"
-        ></v-textarea>
-
-        <!-- Tooltip for Copy message -->
-        <v-layout justify-end>
-          <v-flex xs1>
-            <v-tooltip v-model="showWalletCopyMessage" bottom>
-              <template v-slot:activator="{ on }">
-                <span v-on="on"></span>
-              </template>
-              <span>Wallet address copied to clipboard</span>
-              <v-icon small right @click="showWalletCopyMessage=false">close</v-icon>
-            </v-tooltip>
-          </v-flex>
-        </v-layout>
-
+      <v-flex xs10>
+        <PublicKey :publicKey="getPublicKey(privateKeyObj.privateKey)" />
       </v-flex>
-
-      <v-flex xs1 pt-4>
-        <v-icon small @click="copyWalletAddress">mdi-content-copy</v-icon>
-        <v-icon small @click="showQrcode('WalletAddress')">mdi-barcode</v-icon>
-      </v-flex>
-
     </v-layout>
-
-    <!-- QR-Code dialog -->
-    <div class="text-xs-center">
-      <v-dialog
-        v-model="qrcodeDialog"
-        width="500"
-      >
-        <v-card>
-          <v-card-title
-            class="headline orange justify-center"
-            primary-title
-          >
-            {{ qrcodeTitle }}
-          </v-card-title>
-
-          <v-card-text class="text-xs-center" >
-            <qrcode-vue :value="qrcodeValue" :size="300" level="H"></qrcode-vue>
-          </v-card-text>
-
-          <v-divider></v-divider>
-
-          <v-card-actions class="hidden-print-only">
-            <v-spacer></v-spacer>
-            <v-btn
-              color="orange"
-              flat
-              @click="printQrcode"
-            >
-              Print
-            </v-btn>
-            <v-btn
-              color="primary"
-              flat
-              @click="qrcodeDialog = false"
-            >
-              Close
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </div>
   </v-container>
 </template>
 
 <script>
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
-import QrcodeVue from 'qrcode.vue'
+import PrivateKey from './PrivateKey';
+import PublicKey from './PublicKey';
 
 export default {
   components: {
-    QrcodeVue,
+    PrivateKey,
+    PublicKey,
   },
 
   props: {
@@ -147,14 +55,6 @@ export default {
   data () {
     return {
       checkbox: false,
-      show1: false,
-      password: this.privateKeyObj.privateKey,
-      showPrivateKey: false,
-      showPrivateKeyCopyMessage: false,
-      showWalletCopyMessage: false,
-      qrcodeDialog: false,
-      qrcodeTitle: '',
-      qrcodeValue: '',
     }
   },
 
@@ -182,45 +82,6 @@ export default {
       return keys.getPublic('hex');
     },
 
-    copyPrivateKey () {
-      try {
-        this.$copyText(this.privateKeyObj.privateKey);
-        this.showPrivateKeyCopyMessage = true;
-        setTimeout(() => {
-          this.showPrivateKeyCopyMessage = false
-        }, 3000);
-      } catch (error) {
-        alert('Could not copy sender to clipboard');
-      }
-    },
-
-    copyWalletAddress () {
-      try {
-        this.$copyText(this.getPublicKey(this.privateKeyObj.privateKey));
-        this.showWalletCopyMessage = true;
-        setTimeout(() => {
-          this.showWalletCopyMessage = false
-        }, 3000);
-      } catch (error) {
-        alert('Could not copy sender to clipboard');
-      }
-    },
-
-    showQrcode (field) {
-      if (field === 'privateKey') {
-        this.qrcodeTitle = 'QR-Code for Private Key'
-        this.qrcodeValue = this.privateKeyObj.privateKey;
-        this.qrcodeDialog = true;
-      }else {
-        this.qrcodeTitle = 'QR-Code for Wallet Address'
-        this.qrcodeValue = this.getPublicKey(this.privateKeyObj.privateKey);
-        this.qrcodeDialog = true;
-      }
-    },
-
-    printQrcode () {
-      window.print();
-    }
   }
 }
 </script>
