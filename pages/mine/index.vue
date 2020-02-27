e6238bfe76cb86a4b419120aa12f098b933f0bd8
krispea3 <christian.bracher@gmail.com>
2020-02-27 11:39:24 +0800

    Show Dialog minedBlock after mining

--- pages/mine/index.vue
+++ pages/mine/index.vue
@@ -1,17 +1,50 @@
 <template>
   <div>
     <v-layout row wrap xs12 md10 justify-center>
-      <div v-if="isMined">
-        <h3 class="text-xs-center mb-3">Mined Block</h3>
-        <Block :block="lastBlock" :show-transactions="true" />
+      <div class="text-xs-center">
+        <v-dialog
+          v-model="minedDialog"
+          width="700"
+        >
+          <template v-slot:activator="{ on }">
+            
+          </template>
+
+          <v-card>
+            <v-card-title
+              class="headline orange"
+              primary-title
+            >
+              Mined block
+            </v-card-title>
+
+            <v-card-text>
+              <Block :block="lastBlock" :show-transactions="true" />
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="primary"
+                flat
+                @click="minedDialog = false"
+              >
+                Close
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </div>
     </v-layout>
 
     <v-layout row justify-center mb-3>
-      <v-flex xs12 md7>
+      <v-flex xs12 md9>
        <v-btn
         :loading="loading.includes('mine')"
         block
+        outline
         @click="mineBlock" 
         color="success"
       >
@@ -31,7 +64,6 @@
       </v-card>
     </v-layout>
   </div>
-  <!-- </v-container> -->
 </template>
 
 <script>
@@ -46,8 +78,9 @@ export default {
 
   data () {
     return {
-      isMined: false,
+      // isMined: false,
       loading: [],
+      minedDialog: false,
     }
   },
 
@@ -64,7 +97,8 @@ export default {
       this.loading.push('mine');
       try {
         await this.$store.dispatch('mineBlock');
-        this.isMined = true;
+        // this.isMined = true;
+        this.minedDialog = true;
         this.loading = [];
       } catch (error) {
         console.log("Error mining block");
