2cbeb9c57054cec7c019ddc421d3ca83c39fd79d
krispea3 <christian.bracher@gmail.com>
2020-02-27 13:11:57 +0800

    Use Toast only client side. Not available on server

--- middleware/hasWallet.js
+++ middleware/hasWallet.js
@@ -1,8 +1,15 @@
 export default function ({ store, redirect, app}) {
+
   if (store.getters.walletAddress === '') {
-    // app.$toast.error('You have been redirected as you need a wallet to access this page!');
-    let myToast = app.$toast.error("You have been redirected as you need a wallet to access this page!");
-    redirect('/register');
-    myToast.goAway(5000);   
+    if (process.client) {
+      let myToast = app.$toast.error("You have been redirected as you need a wallet to access this page!");
+      redirect('/register');
+      myToast.goAway(5000);
+    }
+    // Toast module is not accessible server side
+    if (process.server) {
+      redirect('/register');
+    }
+       
   }
 }
--- middleware/invalidChain.js
+++ middleware/invalidChain.js
@@ -1,9 +1,27 @@
-export default function ({store, redirect, route}) {
+export default function ({store, redirect, route, app}) {
   if (route.path === '/chain/invalid-block' && store.getters.invalidBlockIndex === '') {
-    redirect('/chain');
+    if (process.client) {
+      let myToast = app.$toast.error("You cannot access /invalid-block page. Your chain is valid");
+      redirect('/chain');
+      myToast.goAway(5000)
+    }
+    // Toast module is not accessible server side
+    if (process.server) {
+      redirect('/chain');
+    }
+    
   }
 
   if (route.path !== '/chain/invalid-block' && store.getters.invalidBlockIndex !== '') {
-    redirect('/chain');
+    if (process.client) {
+      let myToast = app.$toast.error("Your chain is invalid. You must first get a new chain from the Network!");
+      redirect('/chain');
+      myToast.goAway(6000)
+    }
+    // Toast module is not accessible server side
+    if (process.server) {
+      redirect('/chain');
+    }
+    
   }
 }
